1e0cef4e00c85d6cabbb9fcbca4a1ab50e901289
literwave <572354941@qq.com>
2025-12-01 15:36:45 +0800

    bug fix and add reward mod

---
+++ read_config/Global.lua
@@ -0,0 +1,9 @@
+-- author:   liter_wave 
+-- Automatic generation from -->>
+-- excel file  name: ./excel/Global.xlsx
+-- excel sheet name: Global
+return {
+    [1] = {
+        ["value1"] = 2
+    }
+}
