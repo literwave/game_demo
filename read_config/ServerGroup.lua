0be1c6a314f11b6f2778c311196b295728a85361
literwave <572354941@qq.com>
2025-12-16 17:46:58 +0800

    fix fileFold

--- read_config/ServerGroup.lua
+++
@@ -1,9 +0,0 @@
--- author:   liter_wave 
--- Automatic generation from -->>
--- excel file  name: ./excel/Report.xlsx
--- excel sheet name: ServerGroup
-return {
-    [99] = {
-        ["reportId"] = 100
-    }
-}
